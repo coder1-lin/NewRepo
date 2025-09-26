523b882a101aabac12b0df9f3396f7d3315b37e4
coder1-lin <3590799517@qq.com>
2025-09-26 10:31:44 +0800

    Add files via upload

---
+++ 随机种子和冒泡排序.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include<time.h>
+using namespace std;
+int Bubblesort(int* arr, int len);
+int printarr(int* arr, int len);
+int main(void) {
+	int arry[10];
+	srand(time(0));
+	for (int i = 0; i < 10; i++) {
+		arry[i]=rand() % 100;
+	}
+	 int len = sizeof(arry) / sizeof(arry[0]);
+	 for (int i = 0; i < 10; i++) {
+		 cout << arry[i] << ' ';
+	 }
+	 cout << "\n";
+	 Bubblesort( arry,  len);
+	 printarr(arry,len );
+	return 0;
+}
+int Bubblesort(int* arr, int len) {
+	int i, j, temp;
+	for (i = 0; i < len; i++) {
+		for (j = 0; j < len - i - 1; j++)
+			if (arr[j] > arr[j + 1]) {
+				temp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = temp;
+			}
+	}
+	return 0;
+}
+int printarr(int* arr, int len) {
+	int i = 0;
+	for (i = 0; i < len; i++) {
+		cout << *arr<< ' ';
+		arr++;
+	}
+	return 0;
+}
